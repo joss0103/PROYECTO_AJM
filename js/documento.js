7650835c1329fc0dd58091bd2a3527e6be43069c
kkkasei <mar.salazarc@duocuc.cl>
2022-04-30 19:13:43 -0400

    er
    er

--- js/documento.js
+++ js/documento.js
@@ -1,3 +1,5 @@
+/* Espacio Martin*/
+
 function inicio(){
     document.getElementById("enviar").disabled = true;
 }
@@ -9,7 +11,7 @@ function v_nombre(){
         document.getElementById("texto_nombre").style.color = "red";
         document.getElementById("flag_nombre").value = "0";
     }else{
-        document.getElementById("texto_nombre").innerHTML = "Correcto";
+        document.getElementById("texto_nombre").innerHTML = "Correcto!";
         document.getElementById("texto_nombre").style.color = "green";
         document.getElementById("flag_nombre").value = "1";
     }
@@ -30,7 +32,7 @@ function v_apellido(){
         document.getElementById("texto_apellido").style.color = "red";
         document.getElementById("flag_apellido").value = "0";
     }else{
-        document.getElementById("texto_apellido").innerHTML = "Correcto";
+        document.getElementById("texto_apellido").innerHTML = "Correcto!";
         document.getElementById("texto_apellido").style.color = "green";
         document.getElementById("flag_apellido").value = "1";
     }
@@ -44,30 +46,24 @@ function v_apellido(){
     }
 }
 ;
-function v_gmail(){
-    var arroba_gmail = document.getElementById("gmail").value;
-    if (largo_apellido<3 || largo_apellido > 15){
-        document.getElementById("texto_apellido").innerHTML = "Error! Apellido entre 3 y 15 caracteres!";
-        document.getElementById("texto_apellido").style.color = "red";
-        document.getElementById("flag_apellido").value = "0";
+function v_email(){
+    document.getElementById('email').addEventListener('input', function(){
+    campo = event.target;
+    valido = document.getElementById('emailOK');
+
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+    if ( emailRegex.test(campo.value)) {
+        valido.innerText = "Correcto!";
+        document.getElementById('emailOK').style.color = "green";
+       
     }else{
-        document.getElementById("texto_apellido").innerHTML = "Correcto";
-        document.getElementById("texto_apellido").style.color = "green";
-        document.getElementById("flag_apellido").value = "1";
+        valido.innerText = "Incorrecto!";
+        document.getElementById('emailOK').style.color = "red";
     }
    
-    var v1 = document.getElementById("flag_nombre").value;
-    var v2 = document.getElementById("flag_apellido").value;
-    if (v1 =="1" && v2 =="1"){
-        document.getElementById("enviar").disabled = false;
-    }else{
-        document.getElementById("enviar").disabled = true;
-    }
-}
+    })
 ;
-
-
-
 
 
 
